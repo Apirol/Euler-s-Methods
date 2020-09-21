f5a5af8b2a35624161d47f997767eca0f2698cd0
Redut Anatoly <56587883+Apirol@users.noreply.github.com>
2020-09-21 17:24:38 +0700

    Added normal Solve

--- Numerical Methods Lab1/Main.cpp
+++ Numerical Methods Lab1/Main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-
+#include <math.h>
 
 std::vector <double> x;
 std::vector <double> u;
@@ -10,28 +10,19 @@ std::vector <double> approximation;
 double step;
 void Solve();
 void Input();
+void ClearVectors();
 
 
 int main()
 {
-	Solve();
-	step /= 2;
-	Solve();
-	step /= 2;
-	Solve();
-}
-
-
-void Solve()
-{
 	Input();
-	for (int i = 1;  i < 1 / step; i += 1)
-	{
-		x.push_back(x[i - 1] + step);
-		y.push_back(x[i] * x[i]);
-		u[i] = u[i - 1] + step * ((u[i - 1] / x[i - 1]) + x[i - 1]);
-		approximation[i] = u[i] - y[i];
-	}
+	Solve();
+	ClearVectors();
+	step /= 2;
+	Solve();
+	ClearVectors();
+	step /= 2;
+	Solve();
 }
 
 
@@ -39,14 +30,52 @@ void Input()
 {
 	setlocale(LC_CTYPE, "Rus");
 	double firstX;
-	std::cout << "¬ведите x ";
+	double firstU;
+
+	std::cout << "¬ведите x: ";
 	std::cin >> firstX;
 	x.push_back(firstX);
-	double firstU;
-	std::cout << "¬ведите u ";
+
+	std::cout << "¬ведите u: ";
 	std::cin >> firstU;
 	u.push_back(firstU);
-	std::cout << "¬ведите h ";
+
+	std::cout << "¬ведите шаг: ";
 	std::cin >> step;
+
 	y.push_back(firstX);
+	approximation.push_back(0);
+}
+
+
+void Solve()
+{
+	int numberOfSteps = (1 / step) + 1;
+	for (int i = 1;  i <  numberOfSteps; i += 1)
+	{
+		x.push_back(x[i - 1] + step);
+		y.push_back(x[i] * x[i]);
+		u.push_back(u[i - 1] + step * ((u[i - 1] / x[i - 1]) + x[i - 1]));
+		approximation.push_back(abs(u[i] - y[i]));
+	}
+}
+
+
+void ClearVectors()
+{
+	x.clear();
+	y.clear();
+	u.clear();
+	approximation.clear();
+}
+
+
+void Output()
+{
+/*	int sizeOfVectors = x.size();
+
+	for (int i = 0; i < sizeOfVectors; i++)
+	{
+		x[i]
+	}*/
 }
